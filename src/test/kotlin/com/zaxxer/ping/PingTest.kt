60b7d61a98e40087819a3729c4d7b76e4dd7e03a
Prasad <prasad@lvi.co.jp>
2019-02-28 18:16:44 +0900

    check ipv6 support

--- src/test/kotlin/com/zaxxer/ping/PingTest.kt
+++ src/test/kotlin/com/zaxxer/ping/PingTest.kt
@@ -90,6 +90,7 @@ class PingTest {
 
 
        pinger.ping(PingTarget(InetAddress.getByName("8.8.8.8")))
+       pinger.ping(PingTarget(InetAddress.getByName("::1")))
        pinger.ping(PingTarget(InetAddress.getByName("2001:4860:4860::8888")))
 
 //      for (i in 0..(10 * pingTargets.size)) {
