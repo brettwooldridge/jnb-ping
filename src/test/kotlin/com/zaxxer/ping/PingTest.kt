93b928964f19930562a984e005410b1568d80743
Prasad <prasad@lvi.co.jp>
2019-03-01 19:26:53 +0900

    Add ipv6 support

--- src/test/kotlin/com/zaxxer/ping/PingTest.kt
+++ src/test/kotlin/com/zaxxer/ping/PingTest.kt
@@ -1,18 +1,25 @@
 package com.zaxxer.ping
 
 import com.zaxxer.ping.impl.*
-import com.zaxxer.ping.impl.NativeStatic.Companion.runtime
 import com.zaxxer.ping.impl.util.dumpBuffer
+import jnr.ffi.Platform
 import jnr.ffi.Struct
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import java.io.BufferedReader
 import java.io.IOException
+import java.io.InputStreamReader
+import java.net.Inet6Address
 import java.net.InetAddress
 import java.nio.ByteBuffer
 import java.util.concurrent.Semaphore
 
 class PingTest {
+
+   val runtime:jnr.ffi.Runtime = jnr.ffi.Runtime.getSystemRuntime()!!
+   val platform: Platform = Platform.getNativePlatform()
+   val isBSD = platform.isBSD
 
    @Test
    fun testChecksum() {
@@ -50,20 +57,44 @@ class PingTest {
    }
 
    @Test
+   fun testSizesAndAlignmentsIpv6() {
+      assertEquals(8, Struct.size(Icmp6()))
+      assertEquals(0, Icmp6().icmp6_type.offset())
+      assertEquals(1, Icmp6().icmp6_code.offset())
+      assertEquals(2, Icmp6().icmp6_cksum.offset())
+
+      val linuxSockAddr6 = LinuxSockAddr6(InetAddress.getByName("::1") as Inet6Address)
+      assertEquals(28, Struct.size(linuxSockAddr6))
+      assertEquals(0, linuxSockAddr6.sin6_family.offset())
+      assertEquals(2, linuxSockAddr6.sin6_port.offset())
+      assertEquals(4, linuxSockAddr6.sin6_flowinfo.offset())
+      assertEquals(24, linuxSockAddr6.sin6_scope_id.offset())
+
+      val bsdSockAddr6 = BSDSockAddr6(InetAddress.getByName("::1") as Inet6Address)
+      assertEquals(28, Struct.size(linuxSockAddr6))
+      assertEquals(0, bsdSockAddr6.sin6_len.offset())
+      assertEquals(1, bsdSockAddr6.sin6_family.offset())
+      assertEquals(2, bsdSockAddr6.sin6_port.offset())
+      assertEquals(4, bsdSockAddr6.sin6_flowinfo.offset())
+      assertEquals(24, bsdSockAddr6.sin6_scope_id.offset())
+   }
+
+   @Test
    @Throws(IOException::class)
    fun pingTest1() {
       val semaphore = Semaphore(2)
+      val timeoutTargets = HashSet<PingTarget>()
 
       class PingHandler : PingResponseHandler {
          override fun onResponse(pingTarget : PingTarget, responseTimeSec : Double, byteCount : Int, seq : Int) {
             System.out.printf("  ${Thread.currentThread()} $byteCount bytes from $pingTarget: icmp_seq=$seq time=%1.6f\n", responseTimeSec)
-
             println("  ${Thread.currentThread()} Calling semaphore.release()\n")
             semaphore.release()
          }
 
          override fun onTimeout(pingTarget : PingTarget) {
             println("  ${Thread.currentThread()} Timeout")
+            timeoutTargets.add(pingTarget)
             semaphore.release()
          }
       }
@@ -93,6 +124,58 @@ class PingTest {
       while (pinger.isPendingWork()) Thread.sleep(500)
 
       pinger.stopSelector()
+
+      assertTrue("$timeoutTargets timed out.", timeoutTargets.isEmpty())
+   }
+
+   @Test
+   @Throws(IOException::class)
+   fun pingTestIpv6() {
+      val semaphore = Semaphore(2)
+      val timeoutTargets = HashSet<PingTarget>()
+
+      class PingHandler : PingResponseHandler {
+         override fun onResponse(pingTarget: PingTarget, responseTimeSec: Double, byteCount: Int, seq: Int) {
+            System.out.printf("  ${Thread.currentThread()} $byteCount bytes from ${pingTarget.toString()
+                    .replace("%", "%%")}: icmp_seq=$seq time=%1.6f\n", responseTimeSec)
+            println("  ${Thread.currentThread()} Calling semaphore.release()\n")
+            semaphore.release()
+         }
+
+         override fun onTimeout(pingTarget: PingTarget) {
+            println("  ${Thread.currentThread()} Timeout")
+            timeoutTargets.add(pingTarget)
+            semaphore.release()
+         }
+      }
+
+      val pinger = IcmpPinger(PingHandler())
+
+      val selectorThread = Thread( {pinger.runSelector()})
+      selectorThread.isDaemon = false
+      selectorThread.start()
+
+      val pingTargets = ArrayList<PingTarget>()
+      if (isBSD) {
+         pingTargets.add(PingTarget(InetAddress.getByName("2001:4860:4860::8888")))
+      } else {
+         pingTargets.add(PingTarget(InetAddress.getByName(getIpv6Address())))
+      }
+
+      for (i in 0..(10 * pingTargets.size)) {
+         if (!semaphore.tryAcquire()) {
+            println("$i: Blocking on semaphore.acquire()")
+            semaphore.acquire()
+         }
+         println("$i: Calling pinger.ping(${pingTargets[i % pingTargets.size].inetAddress})")
+         pinger.ping(pingTargets[i % pingTargets.size])
+      }
+
+      while (pinger.isPendingWork()) Thread.sleep(500)
+
+      pinger.stopSelector()
+
+      assertTrue("$timeoutTargets timed out.", timeoutTargets.isEmpty())
    }
 
    @Test
@@ -128,6 +211,12 @@ class PingTest {
       pinger.stopSelector()
 
       assertTrue("Ping didn't timeout as expected.", timedOut)
+   }
+
+   private fun getIpv6Address() : String {
+      val proc = Runtime.getRuntime().exec("hostname -i")
+      val stdInput = BufferedReader(InputStreamReader(proc.inputStream))
+      return stdInput.readLine().split(" ")[0]
    }
 
 }
